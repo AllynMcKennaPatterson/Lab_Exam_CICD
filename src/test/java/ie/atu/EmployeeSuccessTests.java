09392fa1b40c7065c8ac69b8c5d937bf4c3e196b
Allyn <G00392765@atu.ie>
2022-12-07 11:25:53 +0000

    Added success test for employee name and number

--- src/test/java/ie/atu/EmployeeSuccessTests.java
+++ src/test/java/ie/atu/EmployeeSuccessTests.java
@@ -1,4 +1,25 @@
 package ie.atu;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class EmployeeSuccessTests {
+    private Employee myEmployee;
+
+    @BeforeEach
+    void setUp(){
+        myEmployee = new Employee("Allyn", 123456712341);
+    }
+
+    @Test
+    void testName(){
+        assertEquals("Allyn", myEmployee.getEmployeeName());
+    }
+
+    @Test
+    void testNumber(){
+        assertEquals(123456712341, myEmployee.getEmployeeNumber());
+    }
 }
