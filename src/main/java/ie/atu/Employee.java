85fe6fe687b3d53bc2244a9d2c5c1b38063259e8
Allyn <G00392765@atu.ie>
2022-12-07 11:34:44 +0000

    Generated getters

--- src/main/java/ie/atu/Employee.java
+++ src/main/java/ie/atu/Employee.java
@@ -3,4 +3,15 @@ package ie.atu;
 public class Employee {
     private String name;
     private String number;
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+    
 }
