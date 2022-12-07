e4da7c441451f32dc77c16e408c45ed27fd4dfca
Allyn <G00392765@atu.ie>
2022-12-07 11:37:25 +0000

    Created setter with error checks

--- src/main/java/ie/atu/Employee.java
+++ src/main/java/ie/atu/Employee.java
@@ -13,5 +13,22 @@ public class Employee {
     public String getNumber() {
         return number;
     }
-    
+
+    public void setName(String name) {
+        if(name.length() >= 5){
+            this.name = name;
+        }
+        else{
+            throw new IllegalArgumentException("Error: Employee name must have a minimum of 5 characters");
+        }
+    }
+
+    public void setNumber(String number) {
+        if(number.length() != 12){
+            this.number = number;
+        }
+        else{
+            throw new IllegalArgumentException("Error: Employee name must be a unique 12 digit number");
+        }
+    }
 }
